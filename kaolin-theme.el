31f64631e4057d3951e326eaf048e80724872db9
0rdy <andrew1byte@gmail.com>
2017-05-05 14:34:46 +0300

    Bump version.

--- kaolin-theme.el
+++ kaolin-theme.el
@@ -5,7 +5,7 @@
 ;; Author: 0rdy <mail@0rdy.com>
 ;; URL: https://github.com/0rdy/kaolin-theme
 ;; Package-Requires: ((emacs "24"))
-;; Version: 0.6.1
+;; Version: 0.7.1
 
 ;; This program is free software: you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
