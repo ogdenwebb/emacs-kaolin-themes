9e1ea6cfe73a44678a6b0f9af3528b1bdbe671bd
ogdenwebb <andrew1byte@gmail.com>
2017-10-22 21:29:25 +0300

    Final update custom-theme-load-path.

--- kaolin-theme.el
+++ kaolin-theme.el
@@ -136,7 +136,8 @@ otherwise add at the end of the list."
 (when (and (boundp 'custom-theme-load-path) load-file-name)
   (let* ((base (file-name-directory load-file-name))
          (dir (expand-file-name "themes/" base)))
-    (add-to-list 'custom-theme-load-path dir)))
+    (add-to-list 'custom-theme-load-path
+                 (dir))))
 
 (provide 'kaolin-theme)
 
