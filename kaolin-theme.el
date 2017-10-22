e40c1bd28794dba467fb268ac734892f9d7fb667
ogdenwebb <andrew1byte@gmail.com>
2017-10-22 21:20:13 +0300

    Fix load-path dir name.

--- kaolin-theme.el
+++ kaolin-theme.el
@@ -136,8 +136,7 @@ otherwise add at the end of the list."
 (when (and (boundp 'custom-theme-load-path) load-file-name)
   (let* ((base (file-name-directory load-file-name))
          (dir (expand-file-name "themes/" base)))
-    (add-to-list 'custom-theme-load-path
-                 (dir))))
+    (add-to-list 'custom-theme-load-path dir)))
 
 (provide 'kaolin-theme)
 
