e5920d9d6e8226e6795b2d044be6c6cfeb87402d
ogdenwebb <ogdenwebb@gmail.com>
2018-04-17 15:10:18 +0300

    Fix linum-highlight-face.

--- kaolin-themes-lib.el
+++ kaolin-themes-lib.el
@@ -624,9 +624,9 @@
     ;; Linum & nlinum
     (linum                        (:background line-num-bg :foreground line-num-fg :bold nil
                                                :italic nil :underline nil :strike-through nil))
-    (linum-highlight-face         (:inherit 'linum))
     (nlinum-current-line          (:background line-num-bg :foreground line-num-hl :bold bold
                                                :italic nil :underline nil :strike-through nil))
+    (linum-highlight-face         (:inherit 'nlinum-current-line))
     (linum-relative-current-line  (:inherit 'nlinum-current-line))
     (nlinum-relative-current-face (:inherit 'nlinum-current-line))
 
