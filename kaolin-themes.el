e2c12bb3f623e5e32100f59bb8142b567b90e7d6
ogdenwebb <ogdenwebb@gmail.com>
2018-04-23 19:33:47 +0300

    Version bump.

--- kaolin-themes.el
+++ kaolin-themes.el
@@ -5,7 +5,7 @@
 ;; Author: Ogden Webb <ogdenwebb@gmail.com>
 ;; URL: https://github.com/ogdenwebb/emacs-kaolin-themes
 ;; Package-Requires: ((emacs "25.1") (autothemer "0.2.2") (cl-lib "0.6"))
-;; Version: 1.3.2
+;; Version: 1.3.3
 
 ;; This program is free software: you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
