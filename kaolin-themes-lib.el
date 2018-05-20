16247aca0dcafdf45d0832528d01046521ce3286
Davide Masserut <d.masserut@gmail.com>
2018-05-20 22:14:15 +0200

    Add diff-margin-* faces

--- kaolin-themes-lib.el
+++ kaolin-themes-lib.el
@@ -960,6 +960,9 @@
     (diff-hl-insert (:background diff-add))
     (diff-hl-change (:background diff-mod))
     (diff-hl-delete (:background diff-rem))
+    (diff-hl-margin-insert (:background diff-add :foreground "black" :slant 'normal))
+    (diff-hl-margin-change (:background diff-mod :foreground "black" :slant 'normal))
+    (diff-hl-margin-delete (:background diff-rem :foreground "black" :slant 'normal))
 
     ;; Popup
     (popup-face                (:background tooltip-bg :foreground tooltip-fg :bold bold))
