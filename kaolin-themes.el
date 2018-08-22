e971b2a764a96d6929f355bf130b578421a4c1e4
ogdenwebb <ogdenwebb@gmail.com>
2018-08-22 05:45:46 +0300

    Re-format package commentary.

--- kaolin-themes.el
+++ kaolin-themes.el
@@ -26,7 +26,7 @@
 ;; Kaolin themes are based on the pallete that was originally
 ;; inspired by Sierra.vim with adding some extra colors.
 ;;
-;; =======  This package includes the following themes  =======
+;; -------  This package includes the following themes  -------
 ;;
 ;;  * kaolin-dark - a dark jade variant inspired by Sierra.vim.
 ;;  * kaolin-light - light variant of the original kaolin-dark.
@@ -40,20 +40,20 @@
 ;;  * kaolin-mono-dark - almost monochrome dark green Kaolin theme.
 ;;
 ;;
-;; =======  Configuration example  =======
+;; -------  Configuration example  -------
 ;;
 ;; (require 'kaolin-themes)
 ;;
 ;; (load-theme 'kaolin-dark)
 ;;
-;; =======  Custom theme settings  =======
+;; -------  Custom theme settings  -------
 ;;
 ;;  ;; The following set to t by default
 ;;  (setq kaolin-themes-bold t       ; If nil, disable the bold style.
 ;;        kaolin-themes-italic t     ; If nil, disable the italic style.
 ;;        kaolin-themes-underline t) ; If nil, disable the underline style.
 ;;
-;; =======  Some extra theme features, disabled by default  =======
+;; -------  Some extra theme features, disabled by default  -------
 ;;
 ;;  ;; If t, use the wave underline style instead of regular underline.
 ;;  (setq kaolin-themes-underline-wave t)
