d4f300e7e7f3ebf836e37e299c41b5550dad1980
ogdenwebb <ogdenwebb@gmail.com>
2019-06-26 11:04:23 +0300

    Change bg0 and pange face in valley-light. Adress #23.

--- themes/kaolin-valley-light-theme.el
+++ themes/kaolin-valley-light-theme.el
@@ -39,10 +39,13 @@
    (lime2      "#5B7709")
 
    ;; Color vars
+   (bg0 "#FDF7EE")
    (bg1 (if kaolin-valley-light-alt-bg "#FBFBFB" "#FAF2E9"))
    (bg2 (if kaolin-valley-light-alt-bg white0 "#F3E7D3"))
    (bg3 (if kaolin-valley-light-alt-bg white1 "#F0DFCA"))
    (bg4 (if kaolin-valley-light-alt-bg white2 "#EBD7BE"))
+
+   (pane orange9)
 
    ;; TODO
    (fg1 "#5E5854")
